83c510fa295ef499b18ed15279089c832477537b
Stanislav Venkov <slavi104@gmaIl.com>
2014-12-30 11:20:47 +0200

    add database fixes

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'mysql2'
+# this takes a hash of options, almost all of which map directly
+# to the familiar database.yml in rails
+# See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
+client = Mysql2::Client.new(:host => "localhost", :username => "root")
 
 ActiveRecord::Base.establish_connection(
   :adapter  => "mysql2",
