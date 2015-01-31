ba16c71f5f3473eed9f50853e38da8a91414101c
Stanislav Venkov <slavi104@gmaIl.com>
2015-01-31 20:10:37 +0200

    + some visual fixes

--- models/test.rb
+++ models/test.rb
@@ -2,10 +2,11 @@ class Test < ActiveRecord::Base
   has_many :tracks
 
   def self.get_tests(category)
-    @tests = Test.where(:category => category)
+    @tests = Test.where(:category => category) unless category == 'all'
+    @tests = Test.all() if category == 'all'
     result = ''
     @tests.each { |test| 
-      result << "<a href='test?id=#{test.id}'><div><img style='width:200px;' src='#{test.image}'/></div><span class='testTitle'>#{test.title}</span></a>"
+      result << "<a class='test_item' href='test?id=#{test.id}'><div><img class='category_img test_img' style='width:200px;' src='#{test.image}'/></div><span class='testTitle'>#{test.title}</span></a>"
     }
     result
   end
